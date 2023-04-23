40eb5ea27a4ac290ccadf9bee33f837447891673
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-04-23 14:18:24 -0400

    added a types folder and added types for the header component

---
+++ src/types/component-types.ts
@@ -0,0 +1,28 @@
+export interface NavigationLink {
+  name: string;
+  href: string;
+  dropdown?: DropdownLink[];
+}
+
+export interface DropdownLink {
+  name: string;
+  href: string;
+}
+
+export interface HeaderProps {
+  navigationLinks: NavigationLink[];
+  linkClassName: string;
+  hoverClassName: string;
+  activeLinkClassName: string;
+  currentActiveLocation?: string;
+  dropdownBgColor?: string;
+  arrowColor?: string;
+  dropdownBorderColor?: string;
+  textClassName?: string;
+  companyName?: string;
+  companyNameClassName?: string;
+  logo?: string | undefined;
+  logoClassName?: string;
+  alt?: string;
+  onLinkClick?: () => void;
+}
