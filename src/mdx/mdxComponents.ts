4205c4ad841c8f8eaf680aa60ffe9a3c880edf25
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-04-24 18:34:02 -0400

    added a folder and file that groups all the mdxComponents

---
+++ src/mdx/mdxComponents.ts
@@ -0,0 +1,6 @@
+/**
+ * React Components supported by content *.mdx files.
+ *
+ * @todo MDXRemoteProps['components'] is incorrectly typed w/ React 18 - React.ReactNode vs. JSX.Element vs using React.FC
+ */
+export const mdxComponents = {};
