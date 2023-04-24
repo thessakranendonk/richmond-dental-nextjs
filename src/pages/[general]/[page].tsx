39c100c029212d2dbf64b80179f405b1fc571312
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-04-24 18:43:03 -0400

    added a dynamic 'general' page

---
+++ src/pages/[general]/[page].tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { GetStaticProps, GetStaticPaths, NextPage } from "next";
+import { useRouter } from "next/router";
+import {
+  getAllGeneralPagePaths,
+  getGeneralPageData,
+} from "../../lib/general-page";
+import { MDXRemote } from "next-mdx-remote";
+import { mdxComponents } from "../../mdx/mdxComponents";
+
+export interface GeneralListProps {
+  pages: GeneralPageProps[];
+}
+export interface GeneralPageProps {
+  slug: string;
+  matter: GeneralFrontMatter;
+  content: string;
+  page: any;
+}
+
+export interface GeneralFrontMatter {
+  date?: string;
+  category: string;
+  title: string;
+  slug: string;
+}
+
+const GeneralPage: NextPage<GeneralPageProps> = (props) => {
+  const { matter, page } = props;
+
+  return (
+    <>
+      <article className="prose lg:prose-lg text-xl prose-slate max-w-3xl mx-auto pt-4 md:pt-6">
+        <h1 className="text-xl md:text-xl2 pb-12 font-medium font-LEMONMILK text-teal-500">
+          {matter.title}
+        </h1>
+        <MDXRemote {...page.mdxSource} components={mdxComponents as any} />
+      </article>
+    </>
+  );
+};
+
+export default GeneralPage;
+
+export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
+  const requestPageSlug = (params?.page ?? "") as string;
+  const postData = await getGeneralPageData(requestPageSlug);
+
+  return {
+    props: {
+      content: postData.content,
+      matter: postData.matter,
+      page: postData.page,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
+  const paths = locales
+    ? getAllGeneralPagePaths(locales)
+    : getAllGeneralPagePaths(["en"]);
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
