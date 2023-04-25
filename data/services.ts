2e47dfacb6aeade92cc2422c5456214e29f63e9f
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-04-25 14:22:38 -0400

    added data for services, technologies and emergency dental

---
+++ data/services.ts
@@ -0,0 +1,56 @@
+export const RICHMOND_SERVICES: {
+  service: string;
+  description: string;
+  href: string;
+  id: string;
+}[] = [
+  {
+    service: "Teeth Cleaning",
+    href: "/services#teeth-cleaning",
+    id: "teeth-cleansing",
+    description:
+      "It starts here. A nice clean and healthy mouth will and can prevent tooth decay, gum disease and bad breath. Plus, afterwards your smile will be bright and beautiful! Call us if you haven’t had a cleaning and assessment in a while!",
+  },
+  {
+    service: "White Fillings & Cosmetic Procedures",
+    href: "/services#white-fillings",
+    id: "white-fillings",
+    description:
+      "Tooth decay can only be cleaned out and replaced with a product that is similar to tooth structure. White composites will help protect your natural teeth and can also help reduce sensitivity or pain from cold or sweet foods. Cosmetic Crowns or Veneers can also help to achieve even better esthetic results! Call us if you think you need a white composite filling!",
+  },
+  {
+    service: "Dental Implants",
+    href: "/services#teeth-cleaning",
+    id: "teeth-cleaning",
+    description:
+      "Missing a tooth or teeth? Titanium Dental Implants are the solution! They integrate with your natural jaw and gums and can be used to replace all manner of missing teeth. Ask us about this as a treatment option!",
+  },
+  {
+    service: "Invisalign® & Orthodontics",
+    href: "/services#invistalign-orthodontics",
+    id: "invistalign-orthodontics",
+    description:
+      "A lot of people think that Invisalign helps to improve your cosmetics and smile, and they do! But they also play a key role in managing FUNCTIONAL problems with your teeth. They can improve tight spaces that are impossible to clean and reduce the risk of tooth and jaw damage in the future. Ask us about this if you have any questions!",
+  },
+  {
+    service: "Teeth Whitening",
+    href: "/services#teeth-whitening",
+    id: "teeth-whitening",
+    description:
+      "Whiten your teeth to achieve your perfect smile! You know what the best part about teeth whitening is? Once your teeth are white, you will be MORE inclined to keep them as clean as possible, it’s a win-win situation! Not everything however can be whitened so please call us if you need clarification!",
+  },
+];
+
+export const SERVICES_DROPDOWN = [
+  { name: "Teeth Cleaning", href: "/services#teeth-cleaning" },
+  {
+    name: "White Fillings & Cosmetic Procedures",
+    href: "/services#white-fillings",
+  },
+  { name: "Dental Implants", href: "/services#teeth-cleaning" },
+  {
+    name: "Invisalign® & Orthodontics",
+    href: "/services#invistalign-orthodontics",
+  },
+  { name: "Teeth Whitening", href: "/services#teeth-whitening" },
+];
---
+++ data/technology-digital-dentistry.ts
@@ -0,0 +1,47 @@
+export const RICHMOND_TECHNOLOGY: {
+  technology: string;
+  description: string;
+  href: string;
+  id: string;
+}[] = [
+  {
+    technology: "High Resolution Full Frame DSLR Cameras",
+    href: "/technology-and-digital-dentistry#high-resolution-cameras",
+    id: "high-resolution-cameras",
+    description:
+      "Digital dentistry starts with what the eyes can see. We use state of the art DSLR Cameras to not only document the initial and improved state of your oral health, but hopefully through time, to help you understand problem areas so that you can continue at home to maintain your oral health in a positive way.",
+  },
+  {
+    technology: "Intra-Oral 3D Scanning with Trios®",
+    href: "/technology-and-digital-dentistry#intra-oral-3d-scanning",
+    id: "intra-oral-3d-scanning",
+    description:
+      "Ever have that experience where you have to hold your breath while a gooey, smelly, messy product is placed in your mouth to get a mold? With the introduction of highly precise and accurate Intra-Oral 3D Scanning with Trios®, that’s a thing of the past. 3D Scans are as simple as opening and closing – and the best part? You’re implants, crowns, veneers, and Invisalign treatment will be as accurate as possible!",
+  },
+  {
+    technology: "Dental Loupes and Magnification",
+    href: "/technology-and-digital-dentistry#dental-loupes-and-magnification",
+    id: "dental-loupes-and-magnification",
+    description:
+      "NO Dental treatment should be done with just the naked eye. We use 4.5x magnification in order to do everything from your cleaning to your implant placement for the most accurate and reliable treatment possible!",
+  },
+];
+
+export const TECHNOLOGY_DROPDOWN = [
+  {
+    name: "High Resolution Full Frame DSLR Cameras",
+    href: "/technology-and-digital-dentistry#high-resolution-cameras",
+  },
+  {
+    name: "White Fillings & Cosmetic Procedures",
+    href: "/technology-and-digital-dentistry#intra-oral-3d-scanning",
+  },
+  {
+    name: "Intra-Oral 3D Scanning with Trios®",
+    href: "/technology-and-digital-dentistry#intra-oral-3d-scanning",
+  },
+  {
+    name: "Dental Loupes and Magnification",
+    href: "/technology-and-digital-dentistry#dental-loupes-and-magnification",
+  },
+];
