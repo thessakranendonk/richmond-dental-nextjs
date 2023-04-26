e76b22ef2c54563b7f9f6d7645bbec96f2175119
Felix Lai <felix.lai@hotmail.com>
2023-04-26 10:31:52 -0400

    created formData interface

---
+++ src/components/layout/Forms.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import axios from "axios";
+
+interface FormData {
+  firstName: string;
+  lastName: string;
+  preferredName: string;
+  dateOfBirth: string;
+  gender: string;
+  maritalStatus: string;
+  homePhone: string;
+  mobilePhone: string;
+  workPhone: string;
+  ext: string;
+  email: string;
+  referral: string;
+  address: string;
+}
