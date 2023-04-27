be2a93036db34dc88026613abff099c601f0f6cb
Felix Lai <felix.lai@hotmail.com>
2023-04-27 10:02:26 -0400

    added interface for FOrmData

--- src/types/forms-interface.ts
+++ src/types/forms-interface.ts
@@ -9,3 +9,58 @@ export interface InputProps {
   className: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
+
+export interface FormData {
+  firstName: string;
+  lastName: string;
+  preferredName: string;
+  dateOfBirth: string;
+  gender: string;
+  maritalStatus: string;
+  homePhone: string;
+  mobilePhone: string;
+  workPhone: string;
+  ext: string;
+  email: string;
+  referral: string;
+  address: string;
+  suite: string;
+  city: string;
+  province: string;
+  postalCode: string;
+  subscriber: string;
+  subscriberName: string;
+  insuranceCompany: string;
+  insuranceTel: string;
+  planNum: string;
+  subscriberId: string;
+  emerContact: string;
+  emerRelationship: string;
+  emerTel: string;
+  famDocName: string;
+  famDocAddress: string;
+  famDocTel: string;
+  medCheck: string;
+  smoke: string;
+  medConditions: string;
+  otherMedConditons: string;
+  allergies: string;
+  otherAllergies: string;
+  longTermMeds: string;
+  dentalInjection: string;
+  immuneSystem: string;
+  hospital: string;
+  illness: string;
+  otherIllness: string;
+  pregnant: string;
+  visitReason: string;
+  lastVisit: string;
+  nervous: string;
+  lastXray: string;
+  dentalSpecialist: string;
+  gumBleed: string;
+  antibiotics: string;
+  jawPain: string;
+  terms: string;
+  date: string;
+}
