65def9b4d7d5c8464d6822f4814950d2ead5093e
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-04-29 18:01:27 -0400

    Updated data and images in data folders

--- data/technology-digital-dentistry.ts
+++ data/technology-digital-dentistry.ts
@@ -1,27 +1,31 @@
 export const RICHMOND_TECHNOLOGY: {
-  technology: string;
+  service: string;
   description: string;
   href: string;
+  img: string;
   id: string;
 }[] = [
   {
-    technology: "High Resolution Full Frame DSLR Cameras",
+    service: "High Resolution Full Frame DSLR Cameras",
     href: "/technology-and-digital-dentistry#high-resolution-cameras",
     id: "high-resolution-cameras",
+    img: "https://res.cloudinary.com/dybcfr6cd/image/upload/ar_1:1,c_fill,e_art:hokusai,g_auto,w_1000/v1682719836/richmond-dental/DSC5543-1024x683_zdp76h.jpg",
     description:
       "Digital dentistry starts with what the eyes can see. We use state of the art DSLR Cameras to not only document the initial and improved state of your oral health, but hopefully through time, to help you understand problem areas so that you can continue at home to maintain your oral health in a positive way.",
   },
   {
-    technology: "Intra-Oral 3D Scanning with Trios®",
+    service: "Intra-Oral 3D Scanning with Trios®",
     href: "/technology-and-digital-dentistry#intra-oral-3d-scanning",
     id: "intra-oral-3d-scanning",
+    img: "https://res.cloudinary.com/dybcfr6cd/image/upload/v1682719836/richmond-dental/DSC5543-1024x683_zdp76h.jpg",
     description:
       "Ever have that experience where you have to hold your breath while a gooey, smelly, messy product is placed in your mouth to get a mold? With the introduction of highly precise and accurate Intra-Oral 3D Scanning with Trios®, that’s a thing of the past. 3D Scans are as simple as opening and closing – and the best part? You’re implants, crowns, veneers, and Invisalign treatment will be as accurate as possible!",
   },
   {
-    technology: "Dental Loupes and Magnification",
+    service: "Dental Loupes and Magnification",
     href: "/technology-and-digital-dentistry#dental-loupes-and-magnification",
     id: "dental-loupes-and-magnification",
+    img: "https://res.cloudinary.com/dybcfr6cd/image/upload/v1682719836/richmond-dental/DSC5542-1-1024x683_qlrvgu.jpg",
     description:
       "NO Dental treatment should be done with just the naked eye. We use 4.5x magnification in order to do everything from your cleaning to your implant placement for the most accurate and reliable treatment possible!",
   },
