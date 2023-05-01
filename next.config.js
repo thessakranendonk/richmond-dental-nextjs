f9d75dc8e04b777d4a6242d8ac423901170b08eb
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-05-01 18:54:39 -0400

    updated project packages and next.config

--- next.config.js
+++ next.config.js
@@ -1,32 +1,7 @@
-const { i18n } = require("./next-i18next.config");
-
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
   reactStrictMode: true,
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-  trailingSlash: true,
-  webpack: (config) => {
-    config.resolve = {
-      ...config.resolve,
-      fallback: {
-        fs: false,
-        path: false,
-        os: false,
-      },
-    };
-    return config;
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
 };
-
-module.exports = withMDX(nextConfig);
