f88ba707f3a5d701491dbec8aed7bc225dc87423
Felix Lai <felix.lai@hotmail.com>
2023-05-01 11:15:40 -0400

    changed syntax

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
+import nodemailer from "nodemailer";
 
 const app = express();
+
 app.use(bodyParser.json());
-app.use(cors());
 
 app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello from the backend!" });
+  res.send("Hello from the server!");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(3001, () => {
+  console.log("Server listening on port 3001");
 });
