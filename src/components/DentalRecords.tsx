80bfab79a2f06d8716200a70d7d8d69281953f2f
Felix Lai <felix.lai@hotmail.com>
2023-05-01 15:13:57 -0400

    Update DentalRecords.tsx

--- src/components/DentalRecords.tsx
+++ src/components/DentalRecords.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 interface DentalRecordsFormState {
   currentDate: string;
@@ -19,3 +20,45 @@ const initialDentalState: DentalRecordsFormState = {
   releaseStatement: "",
   releaseTerms: "",
 };
+
+const DentalRecords: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="First name"
+        {...register("First name", { required: true, maxLength: 80 })}
+      />
+      <input
+        type="text"
+        placeholder="Last name"
+        {...register("Last name", { required: true, maxLength: 100 })}
+      />
+      <input
+        type="text"
+        placeholder="To Dental Office"
+        {...register("To Dental Office", {
+          required: true,
+          pattern: /^\S+@\S+$/i,
+        })}
+      />
+      <input
+        type="tel"
+        placeholder="Date of Birth"
+        {...register("Date of Birth", { required: true, maxLength: 12 })}
+      />
+
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default DentalRecords;
