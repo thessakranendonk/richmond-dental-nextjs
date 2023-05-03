3e698d9c9cd3cf2bb8f9d9777d361597c812ebb2
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-05-03 16:09:25 -0400

    Updated the ImageGrid components ui

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -1,8 +1,8 @@
-import { images } from "@/pages/photo-gallery";
+import { images } from "@/pages/office-tour";
 
 const ImageGrid: React.FC = () => {
   return (
-    <div className="flex gap-4 w-full mx-auto">
+    <div className="flex flex-wrap justify-center gap-4 w-full mx-auto">
       {images.map((img, i) => (
         <div className="h-auto max-w-full rounded-lg" key={img}>
           <a href={`#${i.toString()}`}>
@@ -10,7 +10,7 @@ const ImageGrid: React.FC = () => {
               id={i.toString()}
               src={img}
               //   className="object-cover w-28 rounded-lg"
-              className=" mask mask-hexagon mask-center z-10 h-32 w-32"
+              className="mask mask-hexagon mask-center object-cover opacity-80 z-10 h-12 w-12 md:h-16 md:w-16 lg:h-24 lg:w-24"
               alt="Our Practice"
             />
           </a>
