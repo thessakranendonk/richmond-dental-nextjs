8fba70e29e90102ca8a75771ebb31d7e018061d3
Felix Lai <felix.lai@hotmail.com>
2023-05-07 12:33:29 -0400

    added state  and scroll

--- src/components/PatientTestimonials.tsx
+++ src/components/PatientTestimonials.tsx
@@ -1,11 +1,28 @@
-export interface PatientTestimonialProps {
-  authorName: string;
-  rating: number;
-  review: string;
-}
+import { createRef, useState } from "react";
+import { PATIENT_TESTIMONIALS } from "../../data/patient-testimonials";
 
-const PatientTestimonialDiv = ({
-  authorName,
-  rating,
-  review,
-}: PatientTestimonialProps) => {};
+const PatientTestimonials: React.FC = () => {
+  const [currentReview, setCurrentReview] = useState<Number>(0);
+
+  const refs = PATIENT_TESTIMONIALS.reduce((acc: any, val, i) => {
+    acc[i] = createRef();
+    return acc;
+  }, {});
+
+  const scrollToReview = (i: number) => {
+    // Set the index of the image we want to see next
+    setCurrentReview(i);
+    refs[i].current.scrollIntoView({
+      //     Defines the transition animation.
+      behavior: "smooth",
+      //      Defines vertical alignment.
+      block: "nearest",
+      //      Defines horizontal alignment.
+      inline: "start",
+    });
+  };
+
+  const totalReviews = PATIENT_TESTIMONIALS.length;
+};
+
+export default PatientTestimonials;
