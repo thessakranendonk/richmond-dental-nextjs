b9a78b7ff8e0afdf6c5156600ccbcc56d6299bb8
Felix Lai <felix.lai@hotmail.com>
2023-05-07 09:42:06 -0400

    handler function

--- src/pages/api/patient-testimonials.ts
+++ src/pages/api/patient-testimonials.ts
@@ -2,3 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { google } from "googleapis";
 
 const placesApi = google.maps.places;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { query } = req.query;
+  const response = await placesApi.textSearch({
+    query: query as string,
+    type: "establishment",
+  });
+}
