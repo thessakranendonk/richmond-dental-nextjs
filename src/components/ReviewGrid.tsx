2e087f9b5dd2b600ada49f86046bd19ab1e6e5d3
Felix Lai <felix.lai@hotmail.com>
2023-05-07 17:53:08 -0400

    Create ReviewGrid.tsx

---
+++ src/components/ReviewGrid.tsx
@@ -0,0 +1,15 @@
+// import { PATIENT_TESTIMONIALS } from "../../data/patient-testimonials"
+
+// const ReviewGrid: React.FC = () => {
+//     return (
+//         <div>
+//             {PATIENT_TESTIMONIALS.map((review, i) => (
+//                 <div key={review}>
+//                     <a href={`#${i.toString()}`}>
+
+//                     </a>
+//                 </div>
+//             ))}
+//         </div>
+//     )
+// }
