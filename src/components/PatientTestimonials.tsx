b766d89eabc5fed885692b43e9758dbb9664ce27
Felix Lai <felix.lai@hotmail.com>
2023-05-07 17:20:57 -0400

    sliderArrow to view reviews

--- src/components/PatientTestimonials.tsx
+++ src/components/PatientTestimonials.tsx
@@ -43,6 +43,19 @@ const PatientTestimonials: React.FC = () => {
   const arrowStyle =
     "absolute text-white text-2xl z-10 bg-black h-10 w-10 rounded-full opacity-75 flex items-center justify-center";
 
+  const sliderControl = (isLeftButton?: boolean) => (
+    <button
+      type="button"
+      onClick={isLeftButton ? previousReview : nextReview}
+      className={`${arrowStyle} ${isLeftButton ? "left-2" : "right-2"}`}
+      style={{ top: "45%" }}
+    >
+      <span role="img" aria-label={`Arrow ${isLeftButton ? "left" : "right"}`}>
+        {isLeftButton ? "◀" : "▶"}
+      </span>
+    </button>
+  );
+
   return <div></div>;
 };
 
