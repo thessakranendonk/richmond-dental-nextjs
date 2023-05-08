0cafa60b339224d18846766c28da84d46833cc96
Felix Lai <felix.lai@hotmail.com>
2023-05-08 18:23:46 -0400

    added svg style and css keyframes

--- src/components/LogoAnimation.tsx
+++ src/components/LogoAnimation.tsx
@@ -12,6 +12,19 @@ const LogoAnimation: React.FC<SvgProps> = ({ width, height, fill }) => {
   return (
     <div>
       <Image src={logo} alt="logo" width={100} height={100} />
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width={width}
+        height={height}
+        className="stroke-current text-transparent stroke-2 fill-none animate-draw"
+      >
+        <path
+          fill={fill}
+          d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm4.7 14.3c-.2.2-.4.2-.6 0L12 13.4l-4.1 3.9c-.2.2-.4.2-.6 0l-.7-.7c-.2-.2-.2-.4 0-.6l4.8-4.8c.2-.2.4-.2.6 0l4.8 4.8c.2.2.2.4 0 .6l-.7.7z"
+        />
+      </svg>
     </div>
   );
 };
