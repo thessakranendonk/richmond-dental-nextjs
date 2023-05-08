2746cd408994fbafe6b020cac9c1121f1fd60c6c
Felix Lai <felix.lai@hotmail.com>
2023-05-08 19:05:07 -0400

    trying to get logo to animate

--- src/components/LogoAnimation.tsx
+++ src/components/LogoAnimation.tsx
@@ -1,7 +1,7 @@
-import logo from "../../public/images/logo.svg";
 import Image from "next/image";
 import React from "react";
 import styles from "../styles/logoAnimations.module.css";
+import logo from "../../public/images/logo.svg";
 
 interface SvgProps {
   width: string;
@@ -11,20 +11,30 @@ interface SvgProps {
 
 const LogoAnimation: React.FC<SvgProps> = ({ width, height, fill }) => {
   return (
-    <div className={styles.container}>
+    <div>
       <Image src="/images/logo.svg" alt="logo" width={100} height={100} />
+
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         width={width}
         height={height}
-        className={`${styles.logo} ${styles.outline}`}
+        className={`${styles.logoSpin} ${styles.outline}`}
+        style={{ animation: `${styles.fadeInOut} 2s ease-in-out infinite` }}
       >
         <path
           fill={fill}
           d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm4.7 14.3c-.2.2-.4.2-.6 0L12 13.4l-4.1 3.9c-.2.2-.4.2-.6 0l-.7-.7c-.2-.2-.2-.4 0-.6l4.8-4.8c.2-.2.4-.2.6 0l4.8 4.8c.2.2.2.4 0 .6l-.7.7z"
+          opacity="0" // hide the original SVG path
         />
       </svg>
+
+      <div className={`${styles.logoContainer}`}>
+        <div
+          className={styles.logoImage}
+          style={{ backgroundImage: `url(${logo.src})` }}
+        />
+      </div>
     </div>
   );
 };
