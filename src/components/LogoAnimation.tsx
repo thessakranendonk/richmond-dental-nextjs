9e5c86d27a21361e786ad37cf4903aa118f073f7
Felix Lai <felix.lai@hotmail.com>
2023-05-11 14:12:43 -0400

    did fade in left animation on logo

--- src/components/LogoAnimation.tsx
+++ src/components/LogoAnimation.tsx
@@ -1,62 +1,40 @@
-import Image from "next/image";
-import React, { useState, useEffect } from "react";
-import "../styles/logoAnimations.module.css";
-import { keyframes } from "@emotion/react";
+import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
+import Image from "next/image";
 
 const fadeIn = keyframes`
   from {
     opacity: 0;
-    transform: translateX(-50%);
+    transform: translateX(-50%)
   }
   to {
     opacity: 1;
-    transform: translateX(0%);
+    transform: translateX(0%)
   }
 `;
 
-const fadeOut = keyframes`
-  from {
-    opacity: 1;
-    transform: translateX(0%);
-  }
-  to {
-    opacity: 0;
-    transform: translateX(-50%);
-  }
+const FadeInImage = styled(Image)`
+  animation: ${fadeIn} 1s ease-in-out;
 `;
 
-const FadeInSvg = styled.svg`
-  animation: ${fadeIn} 2s ease-in-out;
-`;
-
-const FadeOutSvg = styled.svg`
-  animation: ${fadeOut} 2s ease-in-out;
-`;
-
-const LogoAnimation: React.FC = () => {
+const LogoAnimation = () => {
   const [isFadeIn, setIsFadeIn] = useState(false);
-  const [hasPlayedAnimation, setHasPlayedAnimation] = useState(false);
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setIsFadeIn(true);
-    }, 100);
-    return () => clearTimeout(timeoutId);
+    setIsFadeIn(true);
   }, []);
-
-  const handleAnimationEnd = () => {
-    setHasPlayedAnimation(true);
-  };
 
   return (
     <div>
-      <FadeInSvg>
-        <Image src="/images/logo.svg" alt="logo" width={100} height={100} />
-      </FadeInSvg>
-      <FadeOutSvg>
-        <Image src="/images/logo.svg" alt="logo" width={100} height={100} />
-      </FadeOutSvg>
+      {isFadeIn ? (
+        <FadeInImage
+          src="/images/logo.svg"
+          alt="logo"
+          width={100}
+          height={100}
+        />
+      ) : null}
     </div>
   );
 };
