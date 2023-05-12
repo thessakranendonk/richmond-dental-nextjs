e7a6c2d4d7e52a4bfb0eb53c543a6019cf9384f7
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-05-11 21:13:32 -0400

    updated the api/contact route to use pdfkit instead of jspdf after getting package related bugs

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,13 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
-// import createPdf from "../../lib/createPdf";
 import handlebars from "handlebars";
 import path from "path";
 import fs from "fs";
 import { Buffer } from "buffer";
 import PDFDocument from "pdfkit";
+import { alterTextForForm } from "@/lib/functions";
+import { pdfLogo } from "@/lib/pdfLogo";
 
 var pdf = new PDFDocument();
+const date = new Date().toDateString();
 
 const createHTMLToSend = (path: any, replacements: any) => {
   let html = fs.readFileSync(path, {
@@ -35,22 +37,19 @@ const contact = async (req: NextApiRequest, res: NextApiResponse) => {
   const emailPath = path.resolve(templatePath, "emailTemplate.html");
 
   const name = `${firstName}${" "}${lastName}`;
+  const filename = `${firstName}-${lastName}`;
   const replacements = {
     subject: subject,
     name: name,
     email: email,
   };
 
-  // const attachments: { filename: string; content: Buffer }[] = [];
-
   let htmlToSend = createHTMLToSend(emailPath, replacements);
 
   const buffers: any = [];
 
-  pdf.on("data", buffers.push.bind());
-  console.log(req.body);
+  pdf.on("data", buffers.push.bind(buffers));
   pdf.on("end", () => {
-    console.log("end");
     let pdfData = Buffer.concat(buffers);
 
     const transporter = nodemailer.createTransport({
@@ -71,26 +70,37 @@ const contact = async (req: NextApiRequest, res: NextApiResponse) => {
         html: htmlToSend,
         attachments: [
           {
-            // path:
-            // req.headers.referer?.includes("dental-record")
-            //   ? pdfOutputDentalRecord
-            //   : req.headers.referer?.includes("new-patient-form")
-            //   ? pdfOutputNewPatient
-            //   : pdfOutputNewAppointment,
-
-            filename: "attachment.pdf",
+            filename: `${filename}.pdf`,
             content: pdfData,
-
-            // ],
           },
-          // ...attachments,
         ],
       });
     } catch (error: any) {
       return res.status(500).json({ error: error.message || error.toString() });
     }
   });
+
+  pdf.image(pdfLogo, 50, 10, { width: 200, height: 100 });
+
+  pdf.fontSize(30);
+  pdf.text(subject, 50, 130);
+  pdf.fontSize(14);
+  pdf.text("Date:", 50, 170);
+  pdf.fontSize(14);
+  pdf.text(date, 100, 170);
+
+  pdf.list(alterTextForForm(JSON.stringify(req.body)), 50, 200, {
+    align: "left",
+    listType: "bullet",
+    bulletRadius: 0.01,
+  });
+
+  if (patientSig) pdf.text("Patient Signature: ", 50, 470);
+  if (patientSig) pdf.image(patientSig, 50, 500, { width: 200, height: 100 });
+  if (parentSig) pdf.text("Parent Signature: ", 50, 600);
+  if (parentSig) pdf.image(parentSig, 50, 620, { width: 200, height: 100 });
+
+  pdf.end();
   return res.status(200).json({ error: "" });
 };
-
 export default contact;
