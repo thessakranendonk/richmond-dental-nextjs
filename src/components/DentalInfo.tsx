ebb50ff13b0253ba5ed2194c51e32e3f23fab4b1
Felix Lai <felix.lai@hotmail.com>
2023-05-13 17:42:50 -0400

    added style and adjusted widths

--- src/components/DentalInfo.tsx
+++ src/components/DentalInfo.tsx
@@ -4,36 +4,37 @@ import { CiLocationOn } from "react-icons/ci";
 import { AiOutlineClockCircle } from "react-icons/ai";
 
 const DentalInfo = () => {
+  const iconClassName = "mt-1 ml-2 w-4 h-4";
   return (
-    <div>
+    <div className="w-1/3">
       <div className=" flex flex-col justify-evenly ">
         <div className="mt-5 mb-6">
-          <div>
+          <div className="flex flex-row justify-center ">
             <h3>Office</h3>
-            <CiLocationOn />
+            <CiLocationOn className={iconClassName} />
           </div>
           <p>
             500 Richmond St. W, Suite 128 <br /> Toronto, ON, M5V 3N4
           </p>
         </div>
         <div className="mb-6">
-          <div>
+          <div className="flex flex-row justify-center ">
             <h3>E-mail</h3>
-            <AiOutlineMail />
+            <AiOutlineMail className={iconClassName} />
           </div>
           <p>info@richmondwestdental.com</p>
         </div>
         <div className="mb-6">
-          <div>
+          <div className="flex flex-row justify-center ">
             <h3>Phone</h3>
-            <AiOutlinePhone />
+            <AiOutlinePhone className={iconClassName} />
           </div>
           <p>+1 416 366 0777</p>
         </div>
         <div className="mb-6">
-          <div>
+          <div className="flex flex-row justify-center ">
             <h3>Opening Hours</h3>
-            <AiOutlineClockCircle />
+            <AiOutlineClockCircle className={iconClassName} />
           </div>
           <p>
             Mon - Thu: 10 AM - 5 PM <br />
