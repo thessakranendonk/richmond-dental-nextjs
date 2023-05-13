0fda16b0994f455488f73a188bd3ddad5d1c9f8e
Felix Lai <felix.lai@hotmail.com>
2023-05-13 18:21:43 -0400

    added a tag

--- src/components/DentalInfo.tsx
+++ src/components/DentalInfo.tsx
@@ -5,7 +5,7 @@ import { AiOutlineClockCircle } from "react-icons/ai";
 
 const DentalInfo = () => {
   const iconClassName = "mt-1 ml-2 w-4 h-4";
-  const headerClassName = "tracking-widest";
+  const headerClassName = "tracking-widest text-zinc-700";
   return (
     <div className="w-1/3">
       <div className=" flex flex-col justify-evenly ">
@@ -16,9 +16,14 @@ const DentalInfo = () => {
             </h3>
             <CiLocationOn className={iconClassName} />
           </div>
-          <p>
+          <a
+            className="hover:underline text-sm text-zinc-500"
+            href="https://www.google.com/maps?q=500+Richmond+St.+W,+Suite+128,+Toronto,+ON,+M5V+3N4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             500 Richmond St. W, Suite 128 <br /> Toronto, ON, M5V 3N4
-          </p>
+          </a>
         </div>
         <div className="flex flex-col mb-6">
           <div className="flex flex-row  text-center justify-center">
@@ -28,7 +33,10 @@ const DentalInfo = () => {
             <AiOutlineMail className={iconClassName} />
           </div>
           <div>
-            <a href="mailto:info@richmondwestdental.com">
+            <a
+              className="hover:underline text-sm text-zinc-500"
+              href="mailto:info@richmondwestdental.com"
+            >
               info@richmondwestdental.com
             </a>
           </div>
@@ -41,7 +49,7 @@ const DentalInfo = () => {
             </h3>
             <AiOutlinePhone className={iconClassName} />
           </div>
-          <p>+1 416 366 0777</p>
+          <p className="text-sm text-zinc-500">+1 416 366 0777</p>
         </div>
         <div className="mb-6">
           <div className="flex flex-row justify-center ">
@@ -50,7 +58,7 @@ const DentalInfo = () => {
             </h3>
             <AiOutlineClockCircle className={iconClassName} />
           </div>
-          <p className="flex justify-center text-left">
+          <p className="flex justify-center text-left text-sm text-zinc-500">
             Mon - Thu: 10 AM - 5 PM <br />
             Fri: 10 AM - 4 PM <br />
             Every Other Sat: 10 AM - 4 PM <br />
--- src/components/layout/project-layout/Footer.tsx
+++ src/components/layout/project-layout/Footer.tsx
@@ -8,14 +8,16 @@ import DentalInfo from "@/components/DentalInfo";
 const Footer: React.FC<FooterProps> = ({ navigationLinks }) => {
   return (
     <div className="text-center border-t-2 border-zinc-700 w-[calc(10% - 10px)] mx-5">
-      <DentalInfo />
-      <ul className="flex flex-col justify-center text-sm gap-y-2 pb-10 text-zinc-500 mt-5">
-        {navigationLinks.map((link) => (
-          <li key={link.name}>
-            <Link href={link.href}>{snakeCaseToTitleCase(link.name)}</Link>
-          </li>
-        ))}
-      </ul>
+      <div className="flex justify-around mb-5">
+        <ul className="flex flex-col justify-center text-sm gap-y-2 pb-10 text-zinc-500 mt-5">
+          {navigationLinks.map((link) => (
+            <li key={link.name}>
+              <Link href={link.href}>{snakeCaseToTitleCase(link.name)}</Link>
+            </li>
+          ))}
+        </ul>
+        <DentalInfo />
+      </div>
       <div className="mb-12">
         <Button
           extraClassName={clsx(
