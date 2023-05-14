0c586fbcefb372cb10575bea36d9192941efe37a
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-05-14 17:19:20 -0400

    major style updates for home page testimonials
    q
    :q
    :wq

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import PatientTestimonials from "@/components/PatientTestimonials";
-import { PATIENT_TESTIMONIALS } from "../../data/patient-testimonials";
 import LinkCard from "@/components/ui/LinkCard";
-import { RiServiceFill, RiServiceLine } from "react-icons/ri";
+import { RiDoubleQuotesL, RiServiceFill, RiServiceLine } from "react-icons/ri";
 import { HiOfficeBuilding, HiOutlineOfficeBuilding } from "react-icons/hi";
 import { MdCamera, MdOutlineCamera } from "react-icons/md";
 import DentalOffer from "@/components/DentalOffer";
 import Hero from "@/components/Hero";
-
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+import LogoAnimation from "@/components/LogoAnimation";
 
 const iconClassName = "w-24 h-24 text-emerald-700";
 const HomePageLinks = [
@@ -35,15 +37,35 @@ const HomePageLinks = [
     hoverIcon: <HiOfficeBuilding className={iconClassName} />,
   },
 ];
+
+export const fadeIn = {
+  visible: {
+    opacity: 1,
+    transition: { duration: 0.5 },
+    filter: "blur(0)",
+  },
+  hidden: {
+    opacity: 0,
+    filter: "blur(40px)",
+  },
+};
+
 const Home: React.FC = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
   return (
     <div>
       <Hero />
-      <ul className="flex flex-col md:flex-row md:justify-evenly lg:justify-center">
+      <ul className="flex flex-col md:flex-row md:justify-evenly lg:justify-center md: mt-12 lg:mt-24 md:mb-10">
         {HomePageLinks.map((link) => (
           <div
             key={link.href}
-            className="h-96 border-b-[1px] border-zinc-300 last:border-none md:border-none mx-10"
+            className="h-96 border-b-[1px] border-zinc-300 last:border-none md:border-none mx-10 pt-4"
           >
             <LinkCard
               href={link.href}
@@ -55,11 +77,26 @@ const Home: React.FC = () => {
           </div>
         ))}
       </ul>
-      <div>
+      <motion.div
+        ref={ref}
+        viewport={{ once: true }}
+        animate={controls}
+        initial="hidden"
+        variants={fadeIn}
+      >
         <DentalOffer />
-      </div>
-      <div>
-        <PatientTestimonials testimonials={PATIENT_TESTIMONIALS} />
+      </motion.div>
+      <div className="bg-zinc-200 relative mx-auto mt-14 md:mt-48">
+        <div className="relative max-w-md flex justify-center mx-auto">
+          <div className="absolute max-w-md mx-auto text-[4rem] z-20 pt-4">
+            <RiDoubleQuotesL className="text-emerald-500 border-[1px] border-emerald-600 rounded-full bg-white" />
+          </div>
+        </div>
+        <div className="max-w-4xl transform skew-y-[12deg] lg:rounded-xl mx-auto bg-emerald-600/20 bg-clip-content bg-gradient-to-tr from-emerald-600 via-emerald-800 to-emerald-600">
+          <div className="transform -skew-y-[12deg] mb-24">
+            <PatientTestimonials />
+          </div>
+        </div>
       </div>
     </div>
   );
