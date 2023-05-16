dd76b1b66d555ea58b66e6ca78ebcf726fd901f2
Felix Lai <felix.lai@hotmail.com>
2023-05-16 18:24:59 -0400

    trying to add icon marker

--- src/components/BookingMap.tsx
+++ src/components/BookingMap.tsx
@@ -1,10 +1,14 @@
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { LatLngExpression } from "leaflet";
+import { CiLocationOn } from "react-icons/ci";
 
 const BookingMap = () => {
   const position: LatLngExpression = [43.64666, -79.394043]; // Latitude and longitude coordinates
 
+  const markerOptions = {
+    icon: <CiLocationOn />,
+  };
   return (
     <MapContainer
       center={position}
@@ -15,7 +19,7 @@ const BookingMap = () => {
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} />
+      <Marker position={position} {...markerOptions} />
     </MapContainer>
   );
 };
