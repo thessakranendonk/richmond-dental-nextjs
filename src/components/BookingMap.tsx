f0e9321e756cc918a846b0c42a32f9d45909e67a
Felix Lai <felix.lai@hotmail.com>
2023-05-16 17:06:17 -0400

    added lat and lang for richmond west

--- src/components/BookingMap.tsx
+++ src/components/BookingMap.tsx
@@ -2,6 +2,7 @@ import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const BookingMap = () => {
+  const position = [43.64666, -79.394043]; // Latitude and longitude coordinates
   return <div></div>;
 };
 
