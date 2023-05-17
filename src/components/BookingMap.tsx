f8a421a17ddab58cd0c50a177fcf19c8289c6a29
Felix Lai <felix.lai@hotmail.com>
2023-05-17 14:25:30 -0400

    adjusted sizing of map and container

--- src/components/BookingMap.tsx
+++ src/components/BookingMap.tsx
@@ -21,42 +21,44 @@ const BookingMap = () => {
   const headerClassName = "tracking-widest text-sm text-gray-300 pb-1";
   const divClassName = "mt-5 mb-6";
   return (
-    <div className="flex flex-col text-center w-1/2">
-      <div className={divClassName}>
-        <div>
-          <h3 className={headerClassName}>
-            <strong>Office</strong>{" "}
-          </h3>
-          <CiLocationOn className={iconClassName} />
+    <div className="flex justify-center items-center mt-5 w-1/2">
+      <div className="flex flex-col">
+        <div className={divClassName}>
+          <div className="flex flex-row justify-center">
+            <h3 className={headerClassName}>
+              <strong>Office</strong>{" "}
+            </h3>
+            <CiLocationOn className={iconClassName} />
+          </div>
+          <a
+            className=" flex justify-center text-left hover:underline text-sm text-black"
+            href="https://www.google.com/maps?q=500+Richmond+St.+W,+Suite+128,+Toronto,+ON,+M5V+3N4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            500 Richmond St. W, Suite 128 <br /> Toronto, ON, M5V 3N4
+          </a>
         </div>
-        <a
-          className="hover:underline text-sm text-black"
-          href="https://www.google.com/maps?q=500+Richmond+St.+W,+Suite+128,+Toronto,+ON,+M5V+3N4"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          500 Richmond St. W, Suite 128 <br /> Toronto, ON, M5V 3N4
-        </a>
-      </div>
 
-      <div className="mb-6">
-        <div className="flex flex-row justify-center">
-          <h3 className={headerClassName}>
-            <strong>Opening Hours</strong>
-          </h3>
-          <AiOutlineClockCircle className={iconClassName} />
+        <div className="mb-6">
+          <div className="flex flex-row justify-center">
+            <h3 className={headerClassName}>
+              <strong>Opening Hours</strong>
+            </h3>
+            <AiOutlineClockCircle className={iconClassName} />
+          </div>
+          <p className="flex justify-center text-left text-sm text-black">
+            Mon - Thu: 10 AM - 5 PM <br />
+            Fri: 10 AM - 4 PM <br />
+            Every Other Sat: 10 AM - 4 PM <br />
+          </p>
         </div>
-        <p className="flex justify-center text-left text-sm text-black">
-          Mon - Thu: 10 AM - 5 PM <br />
-          Fri: 10 AM - 4 PM <br />
-          Every Other Sat: 10 AM - 4 PM <br />
-        </p>
       </div>
 
       <MapContainer
         center={position}
         zoom={15}
-        style={{ height: "400px", width: "100%" }}
+        style={{ height: "300px", width: "100%" }}
       >
         <TileLayer
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors'
