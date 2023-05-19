3e1959585d9e54ab2f8f7524921d8ff986024c1b
Felix Lai <felix.lai@hotmail.com>
2023-05-19 13:36:05 -0400

    Update DentalInfo.tsx

--- src/components/DentalInfo.tsx
+++ src/components/DentalInfo.tsx
@@ -4,60 +4,44 @@ import { CiLocationOn } from "react-icons/ci";
 import { AiOutlineClockCircle } from "react-icons/ai";
 
 const DentalInfo = () => {
-  const iconClassName = "mt-1 ml-2 w-4 h-4 text-white";
+  const iconClassName = "mt-1 mr-2 w-4 h-4 text-white";
   const headerClassName = "tracking-widest text-sm text-white pb-1";
-  const divClassName = "mt-5 mb-6";
+  const divClassName = " flex flex-row justify-center mt-5 mb-6";
+  const hoursClassName = "flex flex-row justify-between";
   return (
     <div>
       {/* info */}
       <div>
-        <div className=" flex flex-row text-sm">
-          <div className="flex flex-col justify-evenly">
+        <div className="flex text-sm justify-items-start">
+          <div className="flex flex-col">
             <div className={divClassName}>
-              <div className="flex flex-row justify-center ">
-                <h3 className={headerClassName}>
-                  <strong>Phone</strong>
-                </h3>
-                <AiOutlinePhone className={iconClassName} />
-              </div>
+              <AiOutlinePhone className={iconClassName} />
               <p className="text-sm text-white">+1 416 366 0777</p>
             </div>
 
-            <div className="flex flex-col mb-6">
-              <div className="flex flex-row text-center justify-center">
-                <h3 className={headerClassName}>
-                  <strong>E-mail</strong>
-                </h3>
-                <AiOutlineMail className={iconClassName} />
-              </div>
-              <div>
-                <a
-                  className="hover:underline text-sm text-white"
-                  href="mailto:info@richmondwestdental.com"
-                >
-                  info@richmondwestdental.com
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col ml-10">
             <div className={divClassName}>
-              <div className="flex flex-row justify-center ">
-                <h3 className={headerClassName}>
-                  <strong>Office</strong>{" "}
-                </h3>
-                <CiLocationOn className={iconClassName} />
-              </div>
+              <AiOutlineMail className={iconClassName} />
+              <a
+                className="hover:underline text-sm text-white"
+                href="mailto:info@richmondwestdental.com"
+              >
+                info@richmondwestdental.com
+              </a>
+            </div>
+
+            <div className={divClassName}>
+              <CiLocationOn className={iconClassName} />
               <a
                 className="hover:underline text-sm text-white"
                 href="https://www.google.com/maps?q=500+Richmond+St.+W,+Suite+128,+Toronto,+ON,+M5V+3N4"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                500 Richmond St. W, Suite 128 <br /> Toronto, ON, M5V 3N4
+                500 Richmond St. W, Suite 128
               </a>
             </div>
-
+          </div>
+          <div className="flex flex-col ml-10">
             <div className="mb-6">
               <div className="flex flex-row justify-center">
                 <h3 className={headerClassName}>
@@ -65,11 +49,56 @@ const DentalInfo = () => {
                 </h3>
                 <AiOutlineClockCircle className={iconClassName} />
               </div>
-              <p className="flex justify-center text-left text-sm text-white">
-                Mon - Thu: 10 AM - 5 PM <br />
-                Fri: 10 AM - 4 PM <br />
-                Every Other Sat: 10 AM - 4 PM <br />
-              </p>
+              <div>
+                <div className={hoursClassName}>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    Mon
+                  </p>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    10:00 AM - 5:00 PM
+                  </p>
+                </div>
+                <div className={hoursClassName}>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    Tues
+                  </p>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    10:00 AM - 5:00 PM
+                  </p>
+                </div>
+                <div className={hoursClassName}>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    Wed
+                  </p>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    10:00 AM - 5:00 PM
+                  </p>
+                </div>
+                <div className={hoursClassName}>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    Thu
+                  </p>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    10:00 AM - 5:00 PM
+                  </p>
+                </div>
+                <div className={hoursClassName}>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    Fri
+                  </p>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    10:00 AM - 5:00 PM
+                  </p>
+                </div>
+                <div className={hoursClassName}>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    Alt Sat
+                  </p>
+                  <p className="flex justify-center text-left text-sm text-white">
+                    10:00 AM - 4:00 PM
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
