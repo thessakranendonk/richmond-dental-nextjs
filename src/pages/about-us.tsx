f3f0939de94b1e248cd29cb6e1e12d7418215e65
Felix Lai <felix.lai@hotmail.com>
2023-05-25 17:47:30 -0400

    added new about me picture

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -84,7 +84,9 @@ const AboutUs: React.FC = () => {
         </div>
         <img
           className="br-right bg-clip-content text-transparent bg-gradient-to-r from-brand-lightest to-brand-base"
-          src="https://res.cloudinary.com/dybcfr6cd/image/upload/v1682865302/richmond-dental/dr-richmond_wfufcc.png"
+          // src="https://res.cloudinary.com/dybcfr6cd/image/upload/v1682865302/richmond-dental/dr-richmond_wfufcc.png"
+          // src="https://res.cloudinary.com/dyjj9jfc2/image/upload/c_scale,w_552/v1685050606/image-1_tkf616.png"
+          src="https://res.cloudinary.com/dyjj9jfc2/image/upload/c_scale,w_663/v1685050606/image-1_tkf616.png"
         />
       </div>
     </div>
