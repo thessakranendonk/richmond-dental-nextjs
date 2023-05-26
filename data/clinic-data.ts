cb0afd2469a9ad4a61c85876931b6146688e49f3
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-05-25 21:09:56 -0400

    added types and data for footer

---
+++ data/clinic-data.ts
@@ -0,0 +1,20 @@
+import { HoursProps } from "@/types/data-types";
+
+export const HOURS: HoursProps[] = [
+  { day: "Mon", hours: "10:00 AM - 5:00 PM" },
+  { day: "Tue", hours: "10:00 AM - 5:00 PM" },
+  { day: "Wed", hours: "10:00 AM - 5:00 PM" },
+  { day: "Thu", hours: "10:00 AM - 5:00 PM" },
+  { day: "Fri", hours: "10:00 AM - 5:00 PM" },
+  { day: "Alt Sat", hours: "10:00 AM - 4:00 PM" },
+];
+
+export const CLINIC_DETAILS = {
+  clinic_name: "Richmond West Dental",
+  clinic_slogan: "A modern, sophisticated and transparent dental office",
+  neighborhood: "Downtown Toronto | Queen West",
+  location: "Toronto, Ontario",
+  address: "500 Richmond St. W, Suite 128",
+  phone: "+1 416 366 0777",
+  email: "info@richmondwestdental.com",
+};
