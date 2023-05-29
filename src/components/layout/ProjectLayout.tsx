10d805f6ffe1fda7b6cd0ea347d13197ba8e4d65
Felix Lai <felix.lai@hotmail.com>
2023-05-29 12:34:58 -0400

    adjusted screen size and margins

--- src/components/layout/ProjectLayout.tsx
+++ src/components/layout/ProjectLayout.tsx
@@ -41,7 +41,7 @@ export const ProjectLayout: React.FC<PropsWithChildren> = (
         currentActiveLocation={location.pathname}
         textClassName="group text-md transition-all duration-300 ease-in-out text-black font-light mx-8 text-zinc-600"
         linkClassName="flex bg-left-bottom lg:text-md"
-        logoClassName="ml-2 mt-2 mr-1"
+        logoClassName="sm:w-3/5 sm:ml-8 md:w-3/4 ml-2 mt-2 mr-1"
         hoverClassName={clsx(
           "flex bg-left-bottom hover:text-brand-lightest bg-gradient-to-r from-brand-lightest/40 to-brand-darkest bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
         )}
