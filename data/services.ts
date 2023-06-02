17cc3d7e5ba948ebbb0876bab162113745eeac61
Felix Lai <felix.lai@hotmail.com>
2023-06-02 13:59:54 -0400

    replaced photos

--- data/services.ts
+++ data/services.ts
@@ -9,7 +9,7 @@ export const RICHMOND_SERVICES: {
     service: "Teeth Cleaning",
     href: "/services#teeth-cleaning",
     id: "teeth-cleaning",
-    img: "https://res.cloudinary.com/dybcfr6cd/image/upload/v1685490570/richmond-dental/cleanings-square_hrobcy.jpg",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/c_scale,w_900/v1685724965/pexels-daniel-xavier-1239291_ul5anf.jpg",
     // img: "http://res.cloudinary.com/dyjj9jfc2/image/upload/v1685467986/cleanings_zqwvvt.jpg",
 
     description:
@@ -19,7 +19,7 @@ export const RICHMOND_SERVICES: {
     service: "White Fillings & Cosmetic Procedures",
     href: "/services#white-fillings",
     id: "white-fillings",
-    img: "https://res.cloudinary.com/dybcfr6cd/image/upload/v1685491800/richmond-dental/white-filling-square_dl3u7z.jpg",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/c_scale,h_600,w_1100/v1685454630/white-fillings-and-cosmetics_kyfnid.jpg",
 
     description:
       "Tooth decay can only be cleaned out and replaced with a product that is similar to tooth structure. White composites will help protect your natural teeth and can also help reduce sensitivity or pain from cold or sweet foods. Cosmetic Crowns or Veneers can also help to achieve even better esthetic results! Call us if you think you need a white composite filling!",
@@ -28,7 +28,7 @@ export const RICHMOND_SERVICES: {
     service: "Dental Implants",
     href: "/services#dental-implants",
     id: "dental-implants",
-    img: "https://res.cloudinary.com/dybcfr6cd/image/upload/v1685491727/richmond-dental/screw-in-tooth-square_mfuhjd.png",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/c_scale,w_900/v1685454625/dental-implant-downtown-toronto_wh52ov.jpg",
 
     description:
       "Missing a tooth or teeth? Titanium Dental Implants are the solution! They integrate with your natural jaw and gums and can be used to replace all manner of missing teeth. Ask us about this as a treatment option!",
@@ -46,7 +46,7 @@ export const RICHMOND_SERVICES: {
     service: "Teeth Whitening",
     href: "/services#teeth-whitening",
     id: "teeth-whitening",
-    img: "https://res.cloudinary.com/dybcfr6cd/image/upload/v1685491724/richmond-dental/whitening_u8bn8h.jpg",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/c_scale,w_600/v1685724964/michael-dam-mEZ3PoFGs_k-unsplash_okuhbo.jpg",
     description:
       "Whiten your teeth to achieve your perfect smile! You know what the best part about teeth whitening is? Once your teeth are white, you will be MORE inclined to keep them as clean as possible, it’s a win-win situation! Not everything however can be whitened so please call us if you need clarification!",
   },
