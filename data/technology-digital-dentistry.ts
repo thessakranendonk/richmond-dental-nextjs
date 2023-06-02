9104fa8ffe1b2fb53e306b2cec2325be0b16beaf
Felix Lai <felix.lai@hotmail.com>
2023-06-02 14:06:15 -0400

    scaled photos

--- data/technology-digital-dentistry.ts
+++ data/technology-digital-dentistry.ts
@@ -17,7 +17,7 @@ export const RICHMOND_TECHNOLOGY: {
     service: "Intra-Oral 3D Scanning with Trios®",
     href: "/technology-and-digital-dentistry#intra-oral-3d-scanning",
     id: "intra-oral-3d-scanning",
-    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1685655847/3shape-scanner-844p_mzrzvb.png",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/c_scale,w_600/v1685655847/3shape-scanner-844p_mzrzvb.png",
     description:
       "Ever have that experience where you have to hold your breath while a gooey, smelly, messy product is placed in your mouth to get a mold? With the introduction of highly precise and accurate Intra-Oral 3D Scanning with Trios®, that’s a thing of the past. 3D Scans are as simple as opening and closing – and the best part? You’re implants, crowns, veneers, and Invisalign treatment will be as accurate as possible!",
   },
@@ -25,7 +25,7 @@ export const RICHMOND_TECHNOLOGY: {
     service: "Dental Loupes and Magnification",
     href: "/technology-and-digital-dentistry#dental-loupes-and-magnification",
     id: "dental-loupes-and-magnification",
-    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/v1685655847/loupes-1100p_esnypt.jpg",
+    img: "https://res.cloudinary.com/dyjj9jfc2/image/upload/c_scale,w_600/v1685655847/loupes-1100p_esnypt.jpg",
     description:
       "NO Dental treatment should be done with just the naked eye. We use 4.5x magnification in order to do everything from your cleaning to your implant placement for the most accurate and reliable treatment possible!",
   },
