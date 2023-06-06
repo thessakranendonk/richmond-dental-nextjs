705f351966a15fbc83edac94e82889cd502f4078
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-06-05 20:25:40 -0400

    updated next config

--- next.config.js
+++ next.config.js
@@ -4,9 +4,10 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  // env: {
-  //   NEXT_PUBLIC_CONTACT_FORM_RECEIVE_EMAIL:
-  //     process.env.NEXT_PUBLIC_CONTACT_FORM_RECEIVE_EMAIL,
-  //   NEXT_PUBLIC_CONTACT_FORM_PASS: process.env.NEXT_PUBLIC_CONTACT_FORM_PASS,
-  // },
+  env: {
+    NEXT_PUBLIC_CONTACT_FORM_RECEIVE_EMAIL:
+      process.env.NEXT_PUBLIC_CONTACT_FORM_RECEIVE_EMAIL,
+    NEXT_PUBLIC_CONTACT_FORM_PASS: process.env.NEXT_PUBLIC_CONTACT_FORM_PASS,
+    NEXT_PUBLIC_SENDGRID_KEY: process.env.NEXT_PUBLIC_SENDGRID_KEY,
+  },
 };
