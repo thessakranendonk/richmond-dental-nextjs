06031b3e5ab799c5cb683a365a1740fb9b71bc6c
Felix Lai <felix.lai@hotmail.com>
2023-09-08 14:19:49 -0400

    updated with cognito forms

--- src/pages/new-patient-intake.tsx
+++ src/pages/new-patient-intake.tsx
@@ -1,25 +1,17 @@
+import React from "react";
 import PageHeading from "@/components/ui/PageHeading";
+
 const NewPatientIntake: React.FC = () => {
   return (
     <div>
       <div className="relative pt-10 sm:pt-0 h-[14rem] md:h-[18rem]">
         <PageHeading title="Patient Intake Form" />
-      </div>{" "}
+      </div>
       <iframe
-        id="JotFormIFrame-232445360571049"
-        title="Form"
-        onLoad={() => window.parent.scrollTo(0, 0)}
-        allowTransparency={true}
-        allowFullScreen={true}
-        allow="geolocation; microphone; camera"
-        src="https://form.jotform.com/232445360571049"
-        frameBorder="0"
-        style={{
-          minWidth: "100%",
-          maxWidth: "100%",
-          height: "100rem",
-          border: "none",
-        }}
+        src="https://www.cognitoforms.com/f/lNlU0CZ_4kGStt3vQZy0xw/1"
+        style={{ border: "0", width: "100%" }}
+        height="4234"
+        title="Patient Intake Form"
       ></iframe>
     </div>
   );
